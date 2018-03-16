e818c423887da52e0279c9deca9f5831b5f4c8ea
urvaius <urvaius@gmail.com>
2018-03-16 12:38:38 -0700

    eslint setup and not using semi colons

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
+import Vue from 'vue'
 
 const app = new Vue({
   data: {
-    hello: "hi there you 7"
+    hello: 'hi there you 7'
   },
   template: '<div id="app">{{ hello }}</div>'
-});
+})
 
-export { app };
+export { app }
